8a0fd91ad16cda41c6d0ebffa423b0cddd452d13
Zhaldak Anton <kadze009@yandex.ru>
2016-06-30 11:36:42 +0300

    Create project and first class.

---
+++ Zhaldak-Anton/Task1/src/sheduler/CommandController.java
@@ -0,0 +1,10 @@
+package sheduler;
+
+/**
+ * Created by vaka on 30.06.2016.
+ */
+public class CommandController {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
