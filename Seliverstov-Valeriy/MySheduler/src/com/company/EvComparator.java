1602cd58181f84b6bc53a1a456f3ad2109bb2a17
atSeliverstov <arttrackvolt@gmail.com>
2016-07-05 17:31:55 +0300

    some improvements task1
    some improvements task1

--- Seliverstov-Valeriy/MySheduler/src/com/company/EvComparator.java
+++ Seliverstov-Valeriy/MySheduler/src/com/company/EvComparator.java
@@ -10,15 +10,15 @@ import java.util.Comparator;
 public class EvComparator implements Comparator{
     @Override
     public int compare(Object o1, Object o2) {
-        if (((MyEvent) o1).dat.after(((MyEvent) o2).dat)) return 1;
-        if (((MyEvent) o1).dat.equals(((MyEvent) o2).dat)) {
-            if (((MyEvent) o2).info.compareTo(((MyEvent) o1).info) > 0) return 1;
+        if (((MyEvent) o1).getDate().after(((MyEvent) o2).getDate())) return 1;
+        if (((MyEvent) o1).getDate().equals(((MyEvent) o2).getDate())) {
+            if (((MyEvent) o2).getInfo().compareTo(((MyEvent) o1).getInfo()) > 0) return 1;
             else {
-                if (((MyEvent) o2).info.compareTo(((MyEvent) o1).info) < 0) return -1;
+                if (((MyEvent) o2).getInfo().compareTo(((MyEvent) o1).getInfo()) < 0) return -1;
                 else return 0;
             }
         }
-        if (((MyEvent) o1).dat.before(((MyEvent) o2).dat)) return -1;
+        if (((MyEvent) o1).getDate().before(((MyEvent) o2).getDate())) return -1;
         return 0;
     }
 }
--- Seliverstov-Valeriy/MySheduler/src/com/company/Shedule_thr.java
+++ Seliverstov-Valeriy/MySheduler/src/com/company/Shedule_thr.java
@@ -19,15 +19,15 @@ public class Shedule_thr extends Thread {
         while(true) {
             curDate = new Date(System.currentTimeMillis());
             for (int i = 0; i < Global.users.size(); i++)
-                if (Global.users.get(i).stat)
-                    for (int j = 0; j < Global.users.get(i).events.size(); j++)
-                        if ((Global.users.get(i).events.get(j).dat.getTime() < (curDate.getTime()+250)) &&
-                            (Global.users.get(i).events.get(j).dat.getTime() > (curDate.getTime()-250)))
+                if (Global.users.get(i).getStatus())
+                    for (int j = 0; j < Global.users.get(i).sizeEvents(); j++)
+                        if ((Global.users.get(i).getEvent(j).getDate().getTime() < (curDate.getTime()+250)) &&
+                            (Global.users.get(i).getEvent(j).getDate().getTime() > (curDate.getTime()-250)))
                         {
                             System.out.println("!!!!!!!!!!!!!!!!!!!");
                             System.out.println(curDate.toString() + "\n" +
-                                    Global.users.get(i).name + "\n" +
-                                    Global.users.get(i).events.get(j).info);
+                                    Global.users.get(i).getName() + "\n" +
+                                    Global.users.get(i).getEvent(j).getInfo());
                             System.out.println("!!!!!!!!!!!!!!!!!!!");
                         }
             try {
