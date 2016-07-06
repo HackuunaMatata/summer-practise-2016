3acfff730d753ea334c83fe46f54e8a9c9f3a5d1
Zhaldak Anton <kadze009@yandex.ru>
2016-07-06 08:09:05 +0300

    Commit #1 1. Create dir for Task2. 2. Create SQL-script for create Data Base.

---
+++ Zhaldak-Anton/Task2/CreateDB.sql
@@ -0,0 +1,21 @@
+﻿-- СОЗДАНИЕ БАЗЫ ДАННЫХ --
+-- Таблица City --
+CREATE TABLE `City` (
+Name       VARCHAR(40) NOT NULL PRIMARY KEY,
+Population INT NOT NULL,
+Area       INT NOT NULL,
+Country    VARCHAR(40) NOT NULL
+);
+
+INSERT INTO `City` (Name,Population,Area,Country) VALUES ('Moscow', 12000000, 256, 'Russia');
+
+
+-- Таблица Country --
+CREATE TABLE `Country` (
+Name       VARCHAR(40) NOT NULL PRIMARY KEY,
+Population INT NOT NULL,
+Area       INT NOT NULL,
+Capital    VARCHAR(40) NOT NULL
+);
+
+INSERT INTO `Country` (Name,Population,Area,Capital) VALUES ('Russia', 150000000, 17000000, 'Moscow');
