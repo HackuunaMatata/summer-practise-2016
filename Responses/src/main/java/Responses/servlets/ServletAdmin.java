384d73f1a98f4a5c935ec926067f9a51b8235605
LudmilaK <karakchieva.mila@mail.ru>
2016-07-13 02:26:20 +0300

    ServletAdmin is added

---
+++ Responses/src/main/java/Responses/servlets/ServletAdmin.java
@@ -0,0 +1,45 @@
+package Responses.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ServletAdmin extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset=\"utf-8\">");
+        out.println(" <title>HTML Document</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Страница администратора</h1>");
+        out.println("<h2>Настройка парамеров:</h2>");
+        out.println("<ol>");
+        out.println("<li>");
+        out.println("<a href=\"admin_form_questionaries.html\">Редактирование анкеты </a><br>"); // вставить название сервлета
+        out.println("</li>");
+        out.println("<li>");
+        out.println("<a href=\"admin_form_addQuestions.html\">Добавить вопросы в анкету</a><br>");// вставить название сервлета
+        out.println("</li>");
+        out.println("<li>");
+        out.println("<a href=\"admin_form_getData.html\">Получить данные в doc файле</a><br>");// вставить название сервлета
+        out.println("</li>");
+        out.println("<li>");
+        out.println("<a href=\"admin_form_mail.html\"> Настройка уведомлений на e-mail </a><br>");// вставить название сервлета
+        out.println("</li>");
+        out.println("</ol>");
+        out.println("<a href=\"ServletFormsClient\">Вернуться к анкете</a>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
